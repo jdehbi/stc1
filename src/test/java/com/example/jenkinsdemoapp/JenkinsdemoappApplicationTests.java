c776e2473f0c7cae5d5a11e62ad2b9d657337d3a
jdehbi <65607398+jdehbi@users.noreply.github.com>
2021-02-07 13:22:04 +0000

    Update JenkinsdemoappApplicationTests.java

--- src/test/java/com/example/jenkinsdemoapp/JenkinsdemoappApplicationTests.java
+++ src/test/java/com/example/jenkinsdemoapp/JenkinsdemoappApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.jenkinsdemoapp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.example.jenkinsdemoapp.controllers.TestController;
+
+
+@SpringBootTest(classes = JenkinsdemoappApplication.class)
 class JenkinsdemoappApplicationTests {
 
+	@InjectMocks
+	TestController testController;
+
 	@Test
-	void contextLoads() {
+	public void getcontact() throws Exception {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+		ResponseEntity<String> responseEntity = testController.getContactInfo();
+		assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+
 	}
 
 }
